3bae8309ed66e25b70a3fb2233e4a8cb3cc7410c
AhmetEfeYavuz <ahmetefeyavuz@posta.mu.edu.tr>
2025-03-14 12:13:31 +0300

    Java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 /**
  * A simple Tic-Tac-Toe game implementation in Java.
@@ -11,8 +11,35 @@ public class TicTacToe {
 	 * @param args Command line arguments (not used).
 	 */
 	public static void main(String[] args) {
-		// TODO: Initialize the game board and scanner
-		// TODO: Print the initial game board
+		Scanner reader =new Scanner(System.in);
+		char[][] board={{' ',' ',' '},
+					    {' ',' ',' '},
+						{' ',' ',' '}
+
+		};
+		printBoard(board);
+		int validMoves=0;
+		boolean gameEnded=false;
+		int player =0;
+		while (!gameEnded){
+			System.out.println("Player "+player+1 +" enter a row number:");
+			int row = reader.nextInt();
+			System.out.println("Player "+player+1 +" enter a col number:");
+			int col = reader.nextInt();
+			if(isValid(row,col)&&board[row-1][col-1]==' '){
+				board[row-1][col-1]= player==0 ? 'X':'0';
+				printBoard(board);
+				player= ++player %2;
+				validMoves++;
+
+			}else{
+				System.out.println("Please provide valid inputs!!!");
+			}
+			gameEnded = validMoves==9|| checkboard(board,row-1,col-1);
+
+		}
+
+
 		// TODO: Prompt the current player for their move
 		// TODO: Update the board with the player's move
 		// TODO: Handle invalid move
@@ -31,7 +58,47 @@ public class TicTacToe {
 	 * @return True if the current player has won, false otherwise.
 	 */
 	public static boolean checkboard(char[][] board, int row, int col) {
-		// TODO: Implement the logic to check if the current player has won
+		char symbol =board[row][col];
+		boolean win = true;
+		for (int i=0;i<3;i++){
+			if(board[row][i] !=symbol){
+				win=false;
+				break;
+			}
+		}
+		if (win){
+			return true;
+		}
+		for (int i=0;i<3;i++){
+			if (board[i][col] !=symbol );
+			win=false;
+			break;
+		}
+		if (win){
+			return true;
+
+		}
+		if (row==col){
+			win=true;
+			for (int i=0;i<3;i++){
+				if (board[i][i] != symbol){
+					win = false;
+					break;
+				}
+			}if(win){
+				return true;
+			}
+		}if(row+col==2){
+			win = true;
+			for (int i=0;i<3;i++){
+				if (board[2-i][i]!=symbol){
+					win = false;
+					break;
+				}
+			}if (win){
+				return true;
+			}
+		}
         return false;
     }
 
@@ -41,6 +108,20 @@ public class TicTacToe {
 	 * @param board The game board.
 	 */
 	public static void printBoard(char[][] board) {
+		System.out.println("      1	     2	   3");
+		System.out.println("  	---------------------");
+		for (int row =0;row<3;row++){
+			System.out.print(row+1+" ");
+			for (int col=0;col<3;col++){
+				System.out.print("  |");
+				System.out.print(" "+ board[row][col] +" ");
+				if (col==2){
+					System.out.println("  |");
+				}
+			}
+			System.out.println();
+			System.out.println("	---------------------");
+		}
 		// TODO: Implement the logic to print the game board
 	}
 
@@ -52,7 +133,104 @@ public class TicTacToe {
 	 * @return True if the row and column are valid, false otherwise.
 	 */
 	public static boolean isValid(int row, int col) {
-		// TODO: Implement the logic to validate the row and column
+		if(row<1 || row>3){
+			return false;
+		}
+		if (col<1 || col>3){
+			return false;
+		}
 		return false;
 	}
 }
+
+//import java.util.Scanner;
+//
+//public class Main {
+//	static int row;
+//	static int column;
+//	static boolean check;
+//	static String CurrentPlayer;
+//	static String[][] board;
+//	public static void main(String[] args) {
+//
+//		Scanner scanner = new Scanner(System.in);
+//
+//
+//		CurrentPlayer = "X";
+//
+//		board = new String[][]{
+//				{"[ ]", "[ ]", "[ ]"},
+//				{"[ ]", "[ ]", "[ ]"},
+//				{"[ ]", "[ ]", "[ ]"}
+//		};
+//		ShowBoard();
+//		for (int i=0;i<9;i++){
+//			playerchoose();
+//
+//
+//		}
+//	}
+//
+//	static void EndCheck() {
+//		if (board[row][0].equals(board[row][1]) && board[row][0].equals(board[row][2])) {
+//			System.out.println("Game end "+CurrentPlayer+" Won");
+//			System.exit(1);
+//		} else if (board[0][column].equals(board[1][column]) && board[0][column].equals(board[2][column])) {
+//			System.out.println("Game end "+CurrentPlayer+" Won");
+//			System.exit(1);
+//		} else if (!board[0][0].equals("[ ]")&&board[0][0].equals(board[1][1]) && board[2][2].equals(board[0][0])) {
+//			System.out.println("Game end "+CurrentPlayer+" Won");
+//			System.exit(1);
+//		} else if (!board[0][0].equals("[ ]")&&board[2][0].equals(board[1][1]) && board[0][2].equals(board[2][0])) {
+//			System.out.println("Game end "+CurrentPlayer+" Won");
+//			System.exit(1);
+//		}
+//	}
+//
+//
+//	static void ShowBoard() {
+//		for (int i = 0; i < board.length; i++) {
+//			System.out.println();
+//			for (int j = 0; j < board[i].length; j++) {
+//				System.out.print(board[i][j]);
+//			}
+//		}
+//		System.out.println("");
+//	}
+//
+//	static void playerchoose() {
+//		System.out.println("Now playing "+CurrentPlayer);
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter a number to choose a row");
+//		row = scanner.nextInt();
+//		if (row > 2) {
+//			System.out.println("Enter a valid row number that smaller than 3");
+//			row = scanner.nextInt();
+//
+//		}
+//		System.out.println("Enter a number to choose a column");
+//		column = scanner.nextInt();
+//		if (column > 2) {
+//			System.out.println("Enter a valid column number that smaller than 3");
+//			column = scanner.nextInt();
+//		}
+//		BoxCheck();
+//		ShowBoard();
+//		EndCheck();
+//		CurrentPlayer = CurrentPlayer.equals("X") ? "O" : "X";
+//
+//	}
+//	static void BoxCheck(){
+//		check = board[row][column].equals("[X]") || board[row][column].equals("[O]");
+//		if (check){
+//			System.out.println("This box is already choosen");
+//			playerchoose();
+//		}else {
+//			board[row][column]="["+CurrentPlayer+"]";
+//		}
+//
+//	}
+//
+//}
+
+
